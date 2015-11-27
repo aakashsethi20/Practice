b3a3a0bbb10e742c0b68231ded5ac41779a4d9aa
Aakash Sethi <aakashsethi20@gmail.com>
2015-11-27 02:30:42 -0500

    Add bracketsBalance class

--- src/CSI_Lab_02/balance/bracketsBalance.java
+++ src/CSI_Lab_02/balance/bracketsBalance.java
@@ -2,19 +2,58 @@
  *  
  *  Class to check balanced brackets in math expressions  
  *
- *  Usage: java bracketsBalance <exp>
+ *  Usage: java bracketsBalance <expression>
  *  
- *  by Jeff Souza
+ *  by Jeff Souza and Livaniaina Rakotomalala
  *
  */
 
 class bracketsBalance {
 
-    private boolean bBalance (String exp){ 
+    // checks balanced brackets in exp (the expression)
+    private boolean bBalance (String exp){
 
-        // INCLUDE YOUR CODE HERE
+        //Create a new stack
+        ArrayStack stk = new ArrayStack(10);
+        // the bracket pairs
+        String brackets = "(){}[]";
 
-        return false;
+        //Scan across expression
+        for (int i = 0; i < exp.length(); i++){
+            char ch = exp.charAt(i);
+            int inputPos = brackets.indexOf(ch);
+
+            //Push an opening bracket onto the stack
+            if (inputPos % 2 == 0){
+               try{
+                 stk.push (new Character(ch));
+               }
+               catch(StackFullException e){
+               }
+            }
+            //Process a closing bracket
+            else if (inputPos != -1){
+                //If stack empty, then error
+                if (stk.isEmpty())
+                    return false;
+
+                // Retrieve bracket from stack
+                char charFromStack='\0';
+                try{
+                  charFromStack = ((Character)stk.pop()).charValue();
+                }
+                catch(StackEmptyException e){
+               }
+
+                int stackedPos = brackets.indexOf(charFromStack);
+                //If the opening and closing brackets are of different types, then error
+                if (stackedPos+1 != inputPos)
+                    return false;
+            }
+        }
+
+        //If the stack is empty then no error, else error
+        return stk.isEmpty();
 
     }
 
