e0013ab60c5746543c9c5bcd67f8b3099e26e1d9
Aakash Sethi <aakashsethi20@gmail.com>
2015-11-27 00:53:15 -0500

    Add InsertNode

--- src/CSI_Lab_02/dLinkList/DLinkedList.java
+++ src/CSI_Lab_02/dLinkList/DLinkedList.java
@@ -8,36 +8,44 @@
 
 class DLinkedList {
 
-    ListNode firstNode;
-    ListNode lastNode;
+	ListNode firstNode;
+	ListNode lastNode;
 
-    // Appends a node to the end of the list
-    void AppendNode(ListNode nNode) {
-        InsertNode(nNode,lastNode);
-    }
+	// Appends a node to the end of the list
+	void AppendNode(ListNode nNode) {
+		InsertNode(nNode,lastNode);
+	}
 
-    // Inserts a node into the list after pAfter
-    void InsertNode(ListNode nNode, ListNode pAfter) {
+	//********* Implemented this method **************//
+	// Inserts a node into the list after pAfter
+	void InsertNode(ListNode nNode, ListNode pAfter) {
 
-  	  // INSERT YOUR CODE HERE
+		if(pAfter == null) {
+			firstNode = nNode;
+			return;
+		}
+		
+		nNode.previous = pAfter;
+		nNode.next = pAfter.next;
+		nNode.next.previous = nNode;
+		pAfter.next = nNode;
+	}
 
-    }
+	// Removes the specified node from the list
+	void RemoveNode(ListNode nNode) {
 
-    // Removes the specified node from the list
-    void RemoveNode(ListNode nNode) {
+		// INSERT YOUR CODE HERE
 
-  	  // INSERT YOUR CODE HERE
-  
-    }
+	}
 
-    // print the content of the list
-    void print() {
-        ListNode nNode = null;
-        System.out.print("Current list: ");
-        for (nNode = firstNode; nNode != null; nNode = nNode.next) {
-            System.out.print(nNode.data +  " ");
-        }
-        System.out.println("");
-    } 
-  
+	// print the content of the list
+	void print() {
+		ListNode nNode = null;
+		System.out.print("Current list: ");
+		for (nNode = firstNode; nNode != null; nNode = nNode.next) {
+			System.out.print(nNode.data +  " ");
+		}
+		System.out.println("");
+	} 
+
 }
