4551ef485baa39fdc4ca2924c3015852b86bf691
Aakash Sethi <aakashsethi20@gmail.com>
2015-11-27 16:59:17 -0500

    fix isFirst & isLast

--- src/CSI_Lab_03/NodeList.java
+++ src/CSI_Lab_03/NodeList.java
@@ -21,98 +21,108 @@ import java.util.NoSuchElementException;
 import java.util.LinkedList;
 
 public class NodeList<E> {
-  // The linked list which is to be adapted
-  protected LinkedList<E> linkedList = new LinkedList<E>();
+	// The linked list which is to be adapted
+	protected LinkedList<E> linkedList = new LinkedList<E>();
 
-  // Directly available
-  public int size() {
-    return linkedList.size();
-  }
+	// Directly available
+	public int size() {
+		return linkedList.size();
+	}
 
-  // Directly available
-  public boolean isEmpty() {
-     return linkedList.isEmpty();
-  }
+	// Directly available
+	public boolean isEmpty() {
+		return linkedList.isEmpty();
+	}
 
-  // fix me!
-  public boolean isFirst(E element) {
-    return false;
-  }
+	// fixed me!
+	public boolean isFirst(E element) {
+		if(element == null)
+			return false;
+		else if(linkedList.peekFirst() == element)
+			return true;
+		return false;
+	}
 
-  // fix me!
-  public boolean isLast(E element) {
-    return false;
-  }
+	// fixed me!
+	public boolean isLast(E element) {
+		if(element == null)
+			return false;
+		else if(linkedList.peekLast() == element)
+			return true;
+		return false;
+	}
 
-  // Directly available
-  public E first() 
-    throws NoSuchElementException {
-    return linkedList.element();
-  }
-
-
-  // Directly available
-  public E last() 
-    throws NoSuchElementException {
-    return linkedList.getLast();
-  }
-
-  // fix me!
-  E prev(E element)
-    throws NoSuchElementException {
-    throw new NoSuchElementException();
-    // return null;
-  }
+	// Directly available
+	public E first() 
+			throws NoSuchElementException {
+		return linkedList.element();
+	}
 
 
-  // fix me!
-  E next(E element)
-    throws NoSuchElementException {
-    throw new NoSuchElementException();
-    // return null;
-  }
+	// Directly available
+	public E last() 
+			throws NoSuchElementException {
+		return linkedList.getLast();
+	}
 
-  // fix me!
-  public void swapElements(E element1, E element2)
-    throws NoSuchElementException {
-    throw new NoSuchElementException();
-  }
-  
-
-  // fix me!
-  public void set(E currElement, E repElement)
-    throws NoSuchElementException {
-    throw new NoSuchElementException();
-  }
+	// fix me!
+	E prev(E element)
+			throws NoSuchElementException {
+		throw new NoSuchElementException();
+		// return null;
+	}
 
 
-  // Directly available
-  public void addFirst(E element) {
-    linkedList.addFirst(element);
-    return;
-  }
+	// fix me!
+	E next(E element)
+			throws NoSuchElementException {
+		int index = linkedList.indexOf(element) + 1;
+		if(linkedList.get(index) == null)
+			throw new NoSuchElementException();
+		return linkedList.get(index);
+	}
 
-  // Directly available
-  public void addLast(E element) {
-    linkedList.addLast(element);
-    return;
-  }
+	// fix me!
+	public void swapElements(E element1, E element2)
+			throws NoSuchElementException {
+		throw new NoSuchElementException();
+	}
 
-  // fix me!
-  public void addBefore(E currElement,E addElement)
-    throws NoSuchElementException {
-    throw new NoSuchElementException();
-  }
 
-  // fix me!
-  public void addAfter(E currElement,E addElement) 
-    throws NoSuchElementException {
-    throw new NoSuchElementException();
-  }
-  
-  // fix me!
-  public E remove(E element) 
-    throws NoSuchElementException {
-    throw new NoSuchElementException();
-  }
+	// fix me!
+	public void set(E currElement, E repElement)
+			throws NoSuchElementException {
+		throw new NoSuchElementException();
+	}
+
+
+	// Directly available
+	public void addFirst(E element) {
+		linkedList.addFirst(element);
+		return;
+	}
+
+	// Directly available
+	public void addLast(E element) {
+		linkedList.addLast(element);
+		return;
+	}
+
+	// fix me!
+	public void addBefore(E currElement,E addElement)
+			throws NoSuchElementException {
+		throw new NoSuchElementException();
+	}
+
+	// fix me!
+	public void addAfter(E currElement,E addElement) 
+			throws NoSuchElementException {
+		throw new NoSuchElementException();
+	}
+
+	// fix me!
+	public E remove(E element) 
+			throws NoSuchElementException {
+		throw new NoSuchElementException();
+	}
 }
